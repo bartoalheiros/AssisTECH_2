c25ac516954fe7faf30d6669787037c7dde87807
Unknown <alheirosb@gmail.com>
2017-03-02 11:55:37 -0300

    RepositórioTécnicosArrayList
    Status: iniciado.

--- AssisTECH/src/br/ufrpe/assistec/dados/RepositorioTecnicoArrayList.java
+++ AssisTECH/src/br/ufrpe/assistec/dados/RepositorioTecnicoArrayList.java
@@ -1,24 +1,39 @@
 package br.ufrpe.assistec.dados;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.ufrpe.assistec.negocio.beans.Tecnico;
 
 public class RepositorioTecnicoArrayList implements IRepositorioTecnicos {
-
+	List<Tecnico> listaTecnicos = null;
+	
+	public RepositorioTecnicoArrayList() {
+		this.listaTecnicos = new ArrayList();
+	}
+	
 	@Override
 	public void cadastrar(Tecnico tecnico) {
-		// TODO Auto-generated method stub
+		this.listaTecnicos.add(tecnico);
 
 	}
 
 	@Override
 	public Tecnico buscar(String mat) {
-		// TODO Auto-generated method stub
-		return null;
+		Tecnico tecnico = null;
+		
+		for(Tecnico tec: this.listaTecnicos) {
+			if(tec.getMatricula().equals(mat)) {
+				tecnico = tec;
+				break;
+			}
+		}
+		return tecnico;
 	}
 
 	@Override
-	public void listar() {
-		// TODO Auto-generated method stub
+	public List<Tecnico> listar() {
+		
 
 	}
 
