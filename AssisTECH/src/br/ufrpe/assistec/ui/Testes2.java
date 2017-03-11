95e083e23c3ed74127a414a32b2ea651075714d3
Unknown <alheirosb@gmail.com>
2017-03-11 00:06:12 -0300

    RepositorioClientesArrayList
    Já consegue adicionar clientes em arquivo com sucesso.

--- AssisTECH/src/br/ufrpe/assistec/ui/Testes2.java
+++ AssisTECH/src/br/ufrpe/assistec/ui/Testes2.java
@@ -5,24 +5,20 @@ import java.util.List;
 import br.ufrpe.assistec.dados.RepositorioClientesArrayList;
 import br.ufrpe.assistec.negocio.beans.Cliente;
 
-public class Testes {
+public class Testes2 {
 
 	public static void main(String[] args) {
 		RepositorioClientesArrayList repo = RepositorioClientesArrayList.getInstance();
 		
-		Cliente cli4 = new Cliente("1234561", "Norma", "Silva", "Rua das Garças, 72.", "3493-0282", "joao@gmail.com", 2);
-		
-		
-		System.out.println("Adicionando Norma ao Arquivo\n");
-		repo.cadastrar(cli4);
-		
-		
+		System.out.println("Imprimindo todos os Clientes");
+		imprimir(repo.listarTodos());
 
 	}
-
+	
 	public static void imprimir(List<Cliente> listaCli) {
 		for(Cliente cli: listaCli) {
 			System.out.println(cli);
 		}
 	}
+
 }
