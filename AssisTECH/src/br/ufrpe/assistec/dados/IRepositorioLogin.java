c7a037c2df733af61ecc1af88c8bcca1c009187a
Unknown <alheirosb@gmail.com>
2017-03-12 14:12:30 -0300

    RepositorioLoginArrayLIst
    Trabalhando na classe.

--- AssisTECH/src/br/ufrpe/assistec/dados/IRepositorioLogin.java
+++ AssisTECH/src/br/ufrpe/assistec/dados/IRepositorioLogin.java
@@ -1,5 +1,10 @@
 package br.ufrpe.assistec.dados;
 
+import br.ufrpe.assistec.negocio.beans.Login;
+
 public interface IRepositorioLogin {
-	public void adicionar(String usrName, String passwd);
+	public void adicionar(Login lgn);
+	
+	public Login buscar(String usrName);
 }
+
