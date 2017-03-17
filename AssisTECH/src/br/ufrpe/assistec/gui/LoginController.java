c663593e6dce81f331edf98d8bb9434b353b7ecb
Unknown <alheirosb@gmail.com>
2017-03-17 15:05:10 -0300

    Teste

--- AssisTECH/src/br/ufrpe/assistec/gui/LoginController.java
+++ AssisTECH/src/br/ufrpe/assistec/gui/LoginController.java
@@ -1,26 +1,33 @@
 package br.ufrpe.assistec.gui;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import br.ufrpe.assistec.exceptions.NomeDeUsuarioOuSenhaInvalidaException;
 import br.ufrpe.assistec.negocio.ServidorAssisTech;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 
-public class LoginController {
-	@FXML TextField txtUserName;
-	@FXML TextField txtPassWord;
-	@FXML Button btnAcessar;
+public class LoginController implements Initializable{
+	@FXML private TextField txtUserName;
+	@FXML private TextField txtPassWord;
+	@FXML private Button btnAcessar;
+	@FXML private ComboBox privilegio;
 	ServidorAssisTech svr = ServidorAssisTech.getInstance();
 	
 	
@@ -42,5 +49,15 @@ public class LoginController {
 			err.setContentText(e.getMessage());
 			err.showAndWait();
 		}
+		
+		
+	}
+	
+	ObservableList<String> list = FXCollections.observableArrayList("Cliente", "Técnico");
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		privilegio.setItems(list);
+		
 	}
 }
