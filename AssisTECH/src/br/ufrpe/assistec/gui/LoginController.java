3e75428d140cfbbe1cd64c9ceaf3740f069a7d37
Unknown <alheirosb@gmail.com>
2017-07-26 01:56:55 -0300

    Correção Indentação LoginController

--- AssisTECH/src/br/ufrpe/assistec/gui/LoginController.java
+++ AssisTECH/src/br/ufrpe/assistec/gui/LoginController.java
@@ -23,76 +23,77 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+public class LoginController implements Initializable {
 
-public class LoginController implements Initializable{
-	@FXML private TextField txtUserName;
-	@FXML private PasswordField pswdF;
-	@FXML private Button btnAcessar;
-	@FXML private ComboBox<String> tipoUsuario;
-	ServidorAssisTech svr = ServidorAssisTech.getInstance();
+    @FXML
+    private TextField txtUserName;
+    @FXML
+    private PasswordField pswdF;
+    @FXML
+    private Button btnAcessar;
+    @FXML
+    private ComboBox<String> tipoUsuario;
+    ServidorAssisTech svr = ServidorAssisTech.getInstance();
 
+    @FXML
+    public void ckLoginData(ActionEvent event) throws IOException {
+        String usrName = new String(txtUserName.getText());
+        String pswd = new String(pswdF.getText());
 
-	@FXML public void ckLoginData(ActionEvent event) throws IOException {
-		String usrName = new String(txtUserName.getText());
-		String pswd = new String(pswdF.getText());
+        try {
 
+            tipoUsuario.getValue();
 
-		try{
+            if (tipoUsuario.getValue().equals("Cliente")) {
+                try {
 
-			tipoUsuario.getValue();
+                    svr.buscarClientePorLogin(usrName, pswd);
+                    ((Node) event.getSource()).getScene().getWindow().hide();
+                    Parent parent = FXMLLoader.load(getClass().getResource("Janela1.fxml")); //carrega janela1
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(parent);
+                    stage.setScene(scene);
+                    stage.setTitle("AssisTech");
+                    stage.setResizable(false);
+                    stage.setWidth(1222);
+                    stage.setHeight(670);
+                    stage.show();
+                } catch (NomeDeUsuarioOuSenhaInvalidaException e) {
+                    Alert err = new Alert(AlertType.ERROR);
+                    err.setContentText(e.getMessage());
+                    err.showAndWait();
+                }
 
+            } else if (tipoUsuario.getValue().equals("Técnico")) {
+                try {
+                    svr.buscarTecnicoPorLogin(usrName, pswd);
+                    ((Node) event.getSource()).getScene().getWindow().hide();
+                    Parent parent = FXMLLoader.load(getClass().getResource("Janela1.fxml")); //carrega janela1
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(parent);
+                    stage.setScene(scene);
+                    stage.setTitle("AssisTech");
+                    stage.setResizable(false);
+                    stage.show();
+                } catch (NomeDeUsuarioOuSenhaInvalidaException e) {
+                    Alert err = new Alert(AlertType.ERROR);
+                    err.setContentText(e.getMessage());
+                    err.showAndWait();
+                }
+            }
+        } catch (Exception e) {
+            Alert err = new Alert(AlertType.ERROR);
+            err.setContentText("Escolha um tipo de Usuário!");
+            err.showAndWait();
+        }
 
-			if(tipoUsuario.getValue().equals("Cliente")){
-				try {
+    }//Fim do m�todo chkLoginData
 
-					svr.buscarClientePorLogin(usrName, pswd);
-					((Node)event.getSource()).getScene().getWindow().hide();
-					Parent parent = FXMLLoader.load(getClass().getResource("Janela1.fxml")); //carrega janela1
-					Stage stage = new Stage();
-					Scene scene = new Scene(parent);
-					stage.setScene(scene);
-					stage.setTitle("AssisTech");
-					stage.setResizable(false);
-					stage.setWidth(1222);
-					stage.setHeight(670);
-					stage.show();
-				} catch (NomeDeUsuarioOuSenhaInvalidaException e) {
-					Alert err = new Alert(AlertType.ERROR);
-					err.setContentText(e.getMessage());
-					err.showAndWait();
-				}
+    ObservableList<String> list = FXCollections.observableArrayList("Cliente", "Técnico");
 
-			}else if(tipoUsuario.getValue().equals("Técnico")){
-				try {
-					svr.buscarTecnicoPorLogin(usrName, pswd);
-					((Node)event.getSource()).getScene().getWindow().hide();
-					Parent parent = FXMLLoader.load(getClass().getResource("Janela1.fxml")); //carrega janela1
-					Stage stage = new Stage();
-					Scene scene = new Scene(parent);
-					stage.setScene(scene);
-					stage.setTitle("AssisTech");
-					stage.setResizable(false);
-					stage.show();
-				} catch (NomeDeUsuarioOuSenhaInvalidaException e) {
-					Alert err = new Alert(AlertType.ERROR);
-					err.setContentText(e.getMessage());
-					err.showAndWait();
-				}
-			}
-		}catch(Exception e) {
-			Alert err = new Alert(AlertType.ERROR);
-			err.setContentText("Escolha um tipo de Usuário!");
-			err.showAndWait();
-		}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tipoUsuario.setItems(list);
 
-	}//Fim do m�todo chkLoginData
-
-	ObservableList<String> list = FXCollections.observableArrayList("Cliente", "Técnico");
-
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		tipoUsuario.setItems(list);
-
-	}
+    }
 }
